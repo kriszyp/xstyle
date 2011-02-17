a411307d08684a19dc2a876c746bda3870c8724a
Kris Zyp <kriszyp@gmail.com>
2011-02-17 14:37:09 -0700

    make xstyle work standalone

--- xstyle.js
+++ xstyle.js
@@ -1,6 +1,120 @@
 /* Extensible CSS Loader */
-define(["./css", "sizzle"], function(cssLoader, querySelectorAll){
-	cssLoader.createHandle = function(){
+(function(define){
+define(["has", "sizzle"], function(has, querySelectorAll){
+	if(typeof definedCss == "undefined"){
+		definedCss = {};
+	}
+	has.add("event-link-onload", false); /*document.createElement("link").onload === null);*/
+	has.add("dom-create-style-element", !document.createStyleSheet);
+	function insertLink(href){
+		if(has("dom-create-style-element")){
+			// we can use standard <link> element creation
+			styleSheet = document.createElement("link");
+			styleSheet.setAttribute("type", "text/css");
+			styleSheet.setAttribute("rel", "stylesheet");
+			styleSheet.setAttribute("href", href);
+			document.getElementsByTagName("head")[0].appendChild(styleSheet);
+		}
+		return styleSheet;
+	}
+	
+	function insertCss(css){
+		if(has("dom-create-style-element")){
+			// we can use standard <style> element creation
+			styleSheet = document.createElement("style");
+			styleSheet.setAttribute("type", "text/css");
+			styleSheet.appendChild(document.createTextNode(css));
+			document.getElementsByTagName("head")[0].appendChild(styleSheet);
+		}
+		else{
+			try{
+				var styleSheet = document.ceateStyleSheet();
+			}catch(e){
+				// if we went past the 31 stylesheet limit in IE, we will combine all existing stylesheets into one. 
+				var styleSheets = dojox.html.getStyleSheets(); // we would only need the IE branch in this method if it was inlined for other uses
+				var cssText = "";
+				for(var i in styleSheets){
+					var styleSheet = styleSheets[i];
+					if(styleSheet.href){
+						aggregate =+ "@import(" + styleSheet.href + ");";
+					}else{
+						aggregate =+ styleSheet.cssText;
+					}
+					dojo.destroy(styleSheets.owningElement);
+				}
+				var aggregate = dojox.html.getDynamicStyleSheet("_aggregate");
+				aggregate.cssText = cssText;
+				return dojox.html.getDynamicStyleSheet(styleSheetName); 
+			}
+			styleSheet.cssText = css;
+		}
+		return css;
+	}
+	function load(url, loaded){
+	}
+	return {
+		load: function(id, parentRequire, loaded, config){
+			console.log("css loading " + id);
+			if(id.indexOf(".") == -1){
+				id = id + ".css";
+			}
+			var url = parentRequire ? parentRequire.toUrl(id) : id;
+			if(definedCss[url]){
+				// it was defined in the build layer, so we don't need to make any request 
+				// for the CSS, but we need inline it
+				processCss(definedCss[url], url.replace(/[^\/]+$/,''));
+				insertCss(definedCss[url]);
+			}
+			var cssHandle;
+			var callbacks = [];
+			var promise = {
+				then: function(callback){
+					if(cssHandle){
+						callback(cssHandle);
+					}else{
+						callbacks.push(callback);
+					}
+				}
+			};
+			if(!loaded){
+				loaded = function(handle){
+					for(var i = 0;i < callbacks.length; i++){
+						callbacks[i](handle);
+					}
+				};
+			}
+			if(has("event-link-onload")){
+				insertLink(url).onload = function(){
+					cssHandle = this.createHandle();
+					loaded(cssHandle);
+					console.log(url + "loaded");
+				};
+			}else{
+				// need to request the CSS
+				var xhr = typeof XMLHttpRequest == "undefined" ?
+					new ActiveXObject("Microsoft.XMLHTTP") :
+					new XMLHttpRequest;
+				insertLink(url);
+				xhr.open("GET", url, true);
+				var self = this;
+				xhr.onreadystatechange = function(){
+					if(xhr.readyState == 4){
+						if(xhr.status < 400){
+							self.processCss(xhr.responseText, url.replace(/[^\/]+$/,''), function(cssText){
+								cssHandle = this.createHandle();
+								cssHandle.cssText = cssText;
+								loaded(cssHandle);
+							});
+						}else{
+							throw new Error("Unable to load css " + url);
+						}
+					}
+				};
+				xhr.send();
+			}
+			return promise;		
+		},
+		createHandle: function(){
 			return {
 				extend: function(){
 					var css = this.cssText;
@@ -70,7 +184,7 @@ define(["./css", "sizzle"], function(cssLoader, querySelectorAll){
 					// might only need to do this if we have rendering rules
 					require.ready(function(){
 						for(var i in lastRule.renderingRules){
-							lastRule.renderInto(i);
+							lastRule.render(i);
 						}
 					});
 					if(this.cssText != css){
@@ -78,7 +192,7 @@ define(["./css", "sizzle"], function(cssLoader, querySelectorAll){
 						cssLoader.insertCss(css);
 					}
 				},
-				renderInto: function(selector, node){
+				render: function(selector, node){
 					if(typeof selector == "string"){
 						// render the given selector
 						var renderers = this.renderingRules[selector];
@@ -99,6 +213,26 @@ define(["./css", "sizzle"], function(cssLoader, querySelectorAll){
 					
 				}
 			};
-		};		
-	return cssLoader;
+		},
+		setQueryEngine: function(engine){
+			querySelectorAll = engine;
+		},
+		processCss: function (css, baseUrl, loaded){
+			css = css.replace(/\/\*[\s\S]*?\*\//g,'') // remove comments
+						.replace(/(@import\s+[^\s]+\s+)(.+);/g, function(t, rule, query){
+				// TODO: import
+				return t; //return rule + parts.join(" and ") + ';';
+			});
+			loaded(css.replace(/url\("?([^\)"]+)"?\)/g, function(t, url){
+				if(url.charAt(0) != "/"){
+					url = baseUrl + url; 
+				}
+				return "url(" + url + ")";
+			})); 
+		}
+		
+	};
+})
+})(typeof define!="undefined"?define:function(factory){
+	xStyle = factory(has); // create a global if standalone
 });
