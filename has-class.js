d2e3c02b37b925b03aceab312944531287cb1828
Kris Zyp <kriszyp@gmail.com>
2011-08-18 16:34:24 -0600

    Make boolean comparison properly

--- has-class.js
+++ has-class.js
@@ -7,7 +7,7 @@ define(["dojo/has", "./put"], function(has, put){
 				hasResult = has(parts[2]), // the actual has test
 				lower = -parts[4]; // lower bound if it is in the form of test-4 or test-4-6 (would be 4)
 			if((lower > 0 ? lower <= hasResult && (-parts[5] || lower) >= hasResult :  // if it has a range boundary, compare to see if we are in it
-					hasResult) == !parts[1]){ // parts[1] is the no- prefix that can negate the result
+					!!hasResult) == !parts[1]){ // parts[1] is the no- prefix that can negate the result
 				put(document.documentElement, '.has-' + test);
 			}
 		}
