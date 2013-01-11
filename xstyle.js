9e81d92d48c4564f681ece2323d076f5bfc35bee
Kris Zyp <kriszyp@gmail.com>
2013-01-11 14:11:07 -0700

    Refactoring xstyle to use one file and attribute binding

--- xstyle.js
+++ xstyle.js
@@ -13,14 +13,15 @@ if(typeof define == "undefined"){
 		};
 	})();
 }
-define("xstyle/xstyle", ["require"], function (require) {
+define("xstyle/xstyle", ["require", "put-selector/put"], function (require, put) {
 	"use strict";
-	var cssScan = /\s*([^{\}\(\)\/\\'":;]*)(?::\s*([^{\}\(\)\/\\'";]*))?([{\}\(\)\/\\'";]|$)/g;
+	var cssScan = /\s*([^{\}\(\)\/\\'":=;]*)([=:]\s*([^{\}\(\)\/\\'";]*))?([{\}\(\)\/\\'";]|$)/g;
 									// name: value 	operator
 	var singleQuoteScan = /((?:\\.|[^'])*)'/g;
 	var doubleQuoteScan = /((?:\\.|[^"])*)"/g;
 	var commentScan = /\*\//g;
 	var nextId = 0;
+	var globalAttributes = {Math:Math};
 	var undef, testDiv = document.createElement("div");
 	function search(tag){
 		var elements = document.getElementsByTagName(tag);
@@ -187,11 +188,88 @@ console.log("add", selector, cssText);
 					this.addSheetRule(this.selector, this.cssText);
 				}
 			},
-			addProperty: function(name, property){
-				if(!name && property[0].charAt(0) == '='){
-					name = "-x-content";
-					property[0] = property[0].slice(1);
+			recomputeAttribute: function(element, name){
+				this.attributeFunctions[name](element);
+			},
+			get: function(key){
+				// TODO: need to add inheritance?
+				return this.properties[key];
+			},
+			addAttribute: function(name, value){
+				if(value[0].charAt(0) == '>'){
+					value = generate(value, this);
+					if(!name){
+						xstyle.addRenderer("", value, this, value);
+						return;
+					}
+				}else{
+					var target, variables = [], id = 0, variableLength = 0, callbacks = [],
+					parameterized = false;
+					var expression = [];
+					var parts = value.sort ? value : [value];
+					// Do the parsing and function creation just once, and adapt the dependencies for the element at creation time
+					// deal with an array, converting strings to JS-eval'able strings
+					for(var i = 0;i < parts.length;i++){
+						var part = parts[i];
+						expression.push(part instanceof String ? addString(part) : 
+							// find all the variables in the expression
+							part.toString().replace(/[a-zA-Z_$][\w_$\.]*/g, function(variable){
+								var position = id++;
+								// for each reference, we break apart into variable reference and property references after each dot
+								var parts = variable.split('.');
+								variables.push(parts);
+								return addArgument(position);
+								// we will reference the variable a function argument in the function we will create
+							})
+						);
+					}
+					expression = expression.join('');
+					if(expression.length > variableLength){
+						// it's a full expression, so we create a time-varying bound function with the expression
+						var reactiveFunction = new Function('return ' + expression);
+					}
 				}
+				function addString(part){
+					var position = id++;
+					variables[position] = part;
+					return addArgument(position);
+				}
+				function addArgument(position){
+					var replacement = 'arguments[' + position + ']';
+					variableLength += replacement.length;
+					return replacement;
+				}
+				var rule = this; // TODO: can this be passed by addRenderer
+				xstyle.addRenderer(name, value, this, function(element){
+					var waiting = 1, waiting = variables.length, satisfied = [];
+					function recompute(element, setupRule){
+						for(var i = 0; i < variables.length; i++){
+							var parts = variables[i];
+							satisfied[i] = typeof parts.sort ?
+								// TODO: add support for promises
+								findAttributeInAncestors(element, parts[0], setupRule) : parts;
+						}
+						element[name] = reactiveFunction ? reactiveFunction.apply(this, satisfied) : value;
+					}
+					recompute(element, rule);
+					(rule.attributeFunctions || (rule.attributeFunctions = {}))[name] = recompute;
+					/*var each = findAttributeInAncestors(element, "eachFunction");
+					if(each){
+						elementBinding.each = function(item){
+							try{
+								var itemElement = each(element, item);
+								itemElement.item = item;
+								return itemElement;
+							}catch(e){
+								// TODO: use put-selector?
+								console.error(e);
+								element.appendChild(document.createElement('span')).appendChild(document.createTextNode(e));
+							}
+						}
+					}*/
+				});
+			},
+			addProperty: function(name, property){
 				var properties = (this.properties || (this.properties = []));
 				properties.push(name);
 				properties[name] = property;
@@ -289,16 +367,13 @@ console.log("add", selector, cssText);
 						}else{
 							ruleHandled(result);
 						}
-						var noError = true;
-					}finally{
-						if(!noError){
-							handleError();
-						}
+					}catch(e){
+						handleError(e);
 					}
 					function handleError(error){
 						// Add some error handling to give developers a better idea of where error occurs.
 						// TODO: Add line number, and file name
-						console.error("Error occurred processing " + type.slice(2) + ' ' + name + ' in rule ' + rule.selector + ' {' + rule.cssText);
+						console.error('Error occurred processing ' + type.slice(2) + ' ' + name + ' in rule "' + rule.selector + '" {' + rule.cssText);
 						if(error){
 							console.error(error);
 						}
@@ -322,15 +397,20 @@ console.log("add", selector, cssText);
 			cssScan.lastIndex = 0; // start at zero
 			var ruleIndex = 0;
 			while(true){
+				// TODO: use a simplified regex when we are in a property value 
 				var match = cssScan.exec(textToParse);
-				var operator = match[3],
+				var operator = match[4],
 					first = match[1].trim(),
-					value = match[2],
-					name, sequence, assignNextName;
+					assignment = match[2],
+					value = match[3],
+					assignmentOperator, name, sequence, assignNextName;
 					value = value && value.trim();
 				if(assignNextName){
 					// first part of a property
-					name = typeof value == 'string' && first;
+					if(assignment){
+						name = first;
+						assignmentOperator = assignment.charAt(0);
+					}
 					sequence = value = value || first;
 					if(name || operator != '/'){
 						// as long we haven't hit an initial comment, we have the assigned property name now, and don't need to assign again
@@ -338,7 +418,7 @@ console.log("add", selector, cssText);
 					}
 				}else{
 					// subsequent part of a property
-					value = value ? first + ':' + value : first;
+					value = value ? first + assignment : first;
 					addInSequence(value);	
 				}
 				switch(operator){
@@ -416,8 +496,8 @@ console.log("add", selector, cssText);
 							parseSheet(importedSheet.localSource, importedSheet);
 							cssScan.lastIndex = currentIndex;
 						}
-					}else{ 
-						target.addProperty(name, sequence);
+					}else{
+						target[assignmentOperator == ':' ? 'addProperty' : 'addAttribute'](name, sequence);
 					}
 				}
 				name = null;
@@ -461,6 +541,220 @@ console.log("add", selector, cssText);
 	}
 	search('link');
 	search('style');
+
+
+	var rulesListeningToAttribute = {};
+
+	function findAttributeInAncestors(element, name, listeningRule){	
+		var elementAncestor = element;
+		if(listeningRule){
+			var rules = (rulesListeningToAttribute[name] || (rulesListeningToAttribute[name] = []));
+			rules.push(listeningRule); 
+		}
+		do{
+			var value = elementAncestor[name];
+			// if we have a callback, setup a listener
+			if(listeningRule){
+				var oldDescriptor = Object.getOwnPropertyDescriptor(elementAncestor, name);
+				var descriptor, setter = oldDescriptor && oldDescriptor.set;
+				if(!setter){ // only if the a setter hasn't already been defined
+					if(!descriptor){ // create the descriptor just once, and reuse
+						// determine if we should call setAttribute
+						var useSetAttribute = typeof value == 'string';						descriptor = {
+							get: function(){
+								return this._values && this._values[name];
+							},
+							set: function(value){
+								(this._values || (this._values = {}))[name] = value;
+								// set the attribute, the default action
+								useSetAttribute && this.setAttribute(name, value);
+								for(var i = 0; i < rules.length; i++){
+									var rule = rules[i];
+									var nodeList = this.querySelectorAll(rule.selector);
+									for(var j = 0; j < nodeList.length; j++){
+										rule.recomputeAttribute(nodeList[j], name);
+									}
+								}
+							}
+						}
+					}
+					Object.defineProperty(elementAncestor, name, descriptor);
+				}
+			}
+			
+		}while(!value && (elementAncestor != globalAttributes) && 
+				(elementAncestor = elementAncestor.parentNode || globalAttributes));
+		return value;
+	}
+	
+	// elemental section
+	var testDiv = document.createElement("div");
+	var features = {
+		"dom-qsa2.1": !!testDiv.querySelectorAll
+	};
+	function has(feature){
+		return features[feature];
+	}
+	var matchesSelector = testDiv.matchesSelector || testDiv.webkitMatchesSelector || testDiv.mozMatchesSelector || testDiv.msMatchesSelector || testDiv.oMatchesSelector;
+	var selectorRenderers = [];
+	var classHash = {}, propertyHash = {};
+	var renderQueue = [];
+	var documentQueried;
+	require(["dojo/domReady!"], function(){
+		documentQueried = true;
+		if(has("dom-qsa2.1")){
+			for(var i = 0, l = selectorRenderers.length; i < l; i++){
+				findMatches(selectorRenderers[i]);
+			}
+			renderWaiting();
+		}else{
+			var all = document.all;
+			for(var i = 0, l = all.length; i < l; i++){
+				update(all[i]);
+			}
+		}
+	});//else rely on css expressions (or maybe we should use document.all and just scan everything)
+	function findMatches(renderer){
+		// find the elements for a given selector and apply the renderers to it
+		var toRender = [];
+		var results = document.querySelectorAll(renderer.selector);
+		var name = renderer.name;
+		for(var i = 0, l = results.length; i < l; i++){
+			var element = results[i];
+			var currentStyle = element.elementalStyle;
+			var currentSpecificities = element.elementalSpecificities;
+			if(!currentStyle){
+				currentStyle = element.elementalStyle = {};
+				currentSpecificities = element.elementalSpecificities = {};
+			}
+			// TODO: only override if the selector is equal or higher specificity
+			// var specificity = renderer.selector.match(/ /).length;
+			if(true || currentSpecificities[name] <= renderer.specificity){ // only process changes
+				var elementRenderings = element.renderings;
+				if(!elementRenderings){
+					elementRenderings = element.renderings = [];
+					renderQueue.push(element);
+				}
+				
+				elementRenderings.push({
+					name: name,
+					rendered: currentStyle[name] == renderer.propertyValue,
+					renderer: renderer
+				});
+				currentStyle[name] = renderer.propertyValue;
+			} 
+		}
+		
+	}
+	var isCurrent;
+	function renderWaiting(){
+		// render all the elements in the queue to be rendered
+		for(var i = 0; i < renderQueue.length; i++){
+			var element = renderQueue[i];
+			var renderings = element.renderings, currentStyle = element.elementalStyle;
+			delete element.renderings;
+			for(var j = 0; j < renderings.length; j++){
+				var rendering = renderings[j];
+				var renderer = rendering.renderer;
+				var rendered = renderer.rendered;
+				isCurrent = currentStyle[rendering.name] == renderer.propertyValue; // determine if this renderer matches the current computed style
+				if(!rendered && isCurrent){
+					try{
+						renderer.render(element);
+					}catch(e){
+						console.error(e, e.stack);
+						put(element, "div.error", e.toString());
+					}
+				}
+				if(rendered && !isCurrent && renderer.unrender){
+					renderer.unrender(element);
+					renderings.splice(j--, 1); // TODO: need to remove duplicate rendered items as well
+				}
+			}
+		}
+		renderQueue = [];
+	}
+	function apply(element, renderers){
+		// an element was found that matches a selector, so we apply the renderers
+		for(var i = 0, l = renderers.length; i < l; i++){
+			renderers[i](element);
+		}
+	}
+	//window.put = window.put ? put : {};
+	put.onaddclass = function(element, className){
+		var selectorRenderers = classTriggers[className];
+		var renderers = selectorRenderers[selector];
+		for(var i = 0, l = selectorRenderers.length; i < l; i++){
+			var renderer = selectorRenderers[i];
+			if(matchesSelector.apply(element, renderer.selector)){
+				renderer.render(element);
+				(element.renderers = element.renderers || []).push(renderer);
+			}
+		}
+	};
+	put.onremoveclass = function(element){
+		var elementRenderers = element.renderers;
+		if(elementRenderers){
+			for(var i = elementRenderers.length - 1; i >= 0; i--){
+				var renderer = elementRenderers[i];
+				if(!matchesSelector.apply(element, renderer.selector)){
+					renderer.unrender(element);
+					elementRenderers.splice(i, 1);
+				}
+			}
+		}
+	};
+	put.oncreateelement = function(element){
+		tagTriggers[element.tagName]
+	}
+	function update(element){
+	/* At some point, might want to use getMatchedCSSRules for faster access to matching rules 			
+	 	if(typeof getMatchedCSSRules != "undefined"){
+			// webkit gives us fast access to which rules apply
+			getMatchedCSSRules(element);
+		}else{*/
+			for(var i = 0, l = selectorRenderers.length; i < l; i++){
+			var renderer = selectorRenderers[i];
+			if(matchesSelector ?
+					// use matchesSelector if available
+					matchesSelector.call(element, renderer.selector) : // TODO: determine if it is higher specificity that other  same name properties
+					// else use IE's custom css property inheritance mechanism
+					element.currentStyle[renderer.name] == renderer.propertyValue){
+				renderer.render(element);
+			}
+		}
+		
+	}
+
+
+	function generate(value, rule){
+		return function(element, item){
+			var lastElement = element;
+			for(var i = 0, l = value.length;i < l; i++){
+				var part = value[i];
+				if(part.eachProperty){
+					put(lastElement, part.selector);
+					xstyle.update(lastElement);
+				}else if(typeof part == 'string'){
+					if(part.charAt(0) == '='){
+						part = part.slice(1); // remove the '=' at the beginning					
+					}
+					var children = part.split(',');
+					for(var j = 0, cl = children.length;j < cl; j++){
+						var child = children[j].trim();
+						if(child){
+							lastElement = put(j == 0 ? lastElement : element, child);
+							xstyle.update(lastElement);
+						}
+					}
+				}else{
+					lastElement.appendChild(document.createTextNode(part));
+				}
+			}
+			return lastElement;
+		}
+	}
+	
 	var xstyle =  {
 		process: checkImports,
 		vendorPrefix: vendorPrefix,
@@ -492,7 +786,36 @@ console.log("add", selector, cssText);
 				}
 			});
 		},
-		parse: parse
+		parse: parse,
+		
+		addRenderer: function(propertyName, propertyValue, rule, handler){
+			var renderer = {
+				selector: rule.selector,
+				propertyValue: propertyValue,
+				name: propertyName,
+				render: handler
+			};
+			// the main entry point for adding elemental handlers for a selector. The handler
+			// will be called for each element that is created that matches a given selector
+			selectorRenderers.push(renderer);
+			if(documentQueried){
+				findMatches(renderer);
+			}
+			renderWaiting();
+			/*if(!matchesSelector){
+				// create a custom property to identify this rule in created elements
+				return (renderers.triggerProperty = 'selector_' + encodeURIComponent(selector).replace(/%/g, '/')) + ': 1;' +
+					(document.querySelectorAll ? '' : 
+						// we use css expressions for IE6-7 to find new elements that match the selector, since qSA is not available, wonder if it is better to just use document.all...
+						 'zoom: expression(cssxRegister(this,"' + selector +'"));');
+			}*/
+		},
+		update: update, // this should be called for newly created dynamic elements to ensure the proper rules are applied
+		clearRenderers: function(){
+			// clears all the renderers in use
+			selectorRenderers = [];
+		},
+		globalAttributes: globalAttributes		
 	};
 	return xstyle;
 
