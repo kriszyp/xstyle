2ff85ab0913cb5464f8280d4c13a5f876a79a2ce
Kris Zyp <kriszyp@gmail.com>
2013-12-26 07:47:20 -0700

    Update xstyle build

---
+++ build/build-xstyle.bat
@@ -0,0 +1,2 @@
+cat core/amdLoader.js ../put-selector/put.js core/observe.js core/Rule.js core/expression.js core/generate.js core/elemental.js core/base.js core/parser.js main.js > xstyle.js
+uglifyjs xstyle.js -o xstyle.min.js --source-map xstyle.min.js.map -p 2 -c -m
