683b0d95c70e36c87c8e4332837249d555ab4c45
Kris Zyp <kriszyp@gmail.com>
2014-10-10 16:45:55 -0600

    Add support for polling back in for certain old webkit versions

--- core/load-css.js
+++ core/load-css.js
@@ -40,20 +40,33 @@ define([], function(){
 		link.rel = 'stylesheet';
 		link.href = resourceDef;
 		var wait = !options || options.wait !== false;
-		// all browsers support this onload function now
-		link.onload = function(){
-			// cleanup
-			link.onload = null;
-			link.onerror = null;
-			wait && callback(link);
-		};
-		// always add the error handler, so we can notify of any errors
-		link.onerror = function(){
-			// there isn't really any recourse in AMD for errors, so
-			// we just output the error and continue on
-			console.error('Error loading stylesheet ' + resourceDef);
-			wait && callback(link);
-		};
+		// old webkit's would claim to have onload, but didn't really support it
+		var webkitVersion = navigator.userAgent.match(/AppleWebKit\/(\d+\.?\d*)/);
+		webkitVersion = webkitVersion && +webkitVersion[1];
+		if(link.onload === null && !(webkitVersion < 536)){
+			// most browsers support this onload function now
+			link.onload = function(){
+				// cleanup
+				link.onload = null;
+				link.onerror = null;
+				wait && callback(link);
+			};
+			// always add the error handler, so we can notify of any errors
+			link.onerror = function(){
+				// there isn't really any recourse in AMD for errors, so
+				// we just output the error and continue on
+				console.error('Error loading stylesheet ' + resourceDef);
+				wait && callback(link);
+			};
+		}else if(wait){
+			var interval = setInterval(function(){
+				if(link.style){
+					// loaded
+					clearInterval(interval);
+					callback(link);
+				}
+			}, 15);
+		}
 		// add it to the head to trigger loading
 		(head || doc.getElementsByTagName('head')[0]).appendChild(link);
 		if(!wait){
