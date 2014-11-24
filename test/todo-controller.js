e7b3e5b6f98b01526c68c7b4c0bc646cfede4888
Kris Zyp <kriszyp@gmail.com>
2014-11-23 21:54:05 -0700

    Updates to the todo mvc example

--- test/todo-controller.js
+++ test/todo-controller.js
@@ -1,4 +1,9 @@
-define([], function() {
+define(['./todo-model', 'dojo/router'], function(model, router) {
+	router.register('/:view', function(event){
+		var view = event.params.view || 'all';
+		model.tasksView = model[view];
+	});
+	router.startup();
 	return {
 		focusInput: function(taskElement){
 			taskElement.querySelector('.edit').focus();
--- test/todo-model.js
+++ test/todo-model.js
@@ -1,22 +1,33 @@
 define(['dstore/Memory', 'dstore/Trackable'], function(Memory, Trackable){
 	tasks = new (Memory.createSubclass(Trackable))({data:[
-		{id:'one', title:'One'},
-		{id:'two', title:'Two'},
-		{id:'three', title:'Three'}
+		{id:'one', title:'One', completed: false},
+		{id:'two', title:'Two', completed: false},
+		{id:'three', title:'Three', completed: false}
 	]});
+	var activeTasks = tasks.filter({completed: false});
+	var completedTasks = tasks.filter({completed: true});
 	var model = {
 		tasks: tasks,
+		tasksView: tasks,
+		all: tasks,
+		completed: completedTasks,
+		active: activeTasks,
 		newItem: '',
 		addItem: function(event){
 			event.preventDefault();// don't submit the form
 			console.log('add item', model.newItem);
-			tasks.add({title: model.newItem});
+			tasks.add({title: model.newItem, completed: false});
 			model.newItem = '';
 		},
 		destroy: function(item){
 			tasks.remove(item.id);
 		},
-		'total-todo':3
+		totalTodo: 3
 	};
+	tasks.on('add,update,delete', function(event){
+		activeTasks.fetch().totalLength.then(function(totalLength){
+			model.totalTodo = totalLength;	
+		});
+	});
 	return model;
 });
