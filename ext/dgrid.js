45bdd51e5dd677b62c0c5950e55e35f896d02b8c
Kris Zyp <kriszyp@gmail.com>
2014-12-13 07:11:40 -0700

    Update to the latest dgrid API

--- ext/dgrid.js
+++ ext/dgrid.js
@@ -1,4 +1,4 @@
-define(['dojo/_base/declare', 'dojo/promise/all', 'dojo/Deferred', 'dojo/when', './widget', '../core/expression'], function(declare, all, Deferred, when, widgetModule, evaluateExpression){
+define(['dojo/_base/declare', 'dojo/promise/all', 'dojo/Deferred', 'dojo/when', './widget', '../core/expression'], function(declare, all, Deferred, when, widgetModule, expression){
 	var classNeeded = {
 		'selectionMode': 'Selection',
 		'columns': 'Grid',
@@ -11,31 +11,20 @@ define(['dojo/_base/declare', 'dojo/promise/all', 'dojo/Deferred', 'dojo/when', 
 		'keyboard': function(options, value){
 			options.cellNavigation = value == 'cell';
 		},
-		'store': function(options, value, rule){
-			var value = evaluateExpression(rule, 'store', value);
-			when(value, function(value){
-				options.store = value;
+		'collection': function(options, value, rule){
+			value = expression.evaluate(rule, value);
+			return when(value.valueOf(), function(value){
+				options.collection = value;
 			});
-			return value;
 		},
 		'columns': function(options, value){
 			var columns = options.columns = {};
-			var done;
 			value[0].eachProperty(function(name, value){
 				var columnDefinition = widgetModule.parse(value[0]);
 				columnDefinition.className = value[0].selector.slice(1);
-				if(columnDefinition.editor){
-					// create an editor column, wait for it to load
-					done = new Deferred();
-					require(['dgrid/editor'], function (editor) {
-						var parts = columnDefinition.editor.split(/,\s*/);
-						columns[name] = editor(columnDefinition, parts[0], parts[1]);
-						done.resolve();
-					});
-				}
 				columns[name] = columnDefinition;
 			});
-			return done;
+			return columns;
 		}
 	};
 	var waiting;
